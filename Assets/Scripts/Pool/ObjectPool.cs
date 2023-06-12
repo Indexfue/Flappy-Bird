a772450e8f2bc8691a0ead0a972aa416d2eeacb8
Sergei Shalamow <serezha.schalamow@yandex.ru>
2023-06-13 00:30:25 +0500

    add object pool scripts

---
+++ Assets/Scripts/Pool/ObjectPool.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Pool
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] private int _capacity;
+        [SerializeField] private GameObject _container;
+
+        private List<GameObject> _objects = new List<GameObject>();
+
+        protected void Initializate(GameObject prefab)
+        {
+            for (int i = 0; i < _capacity; i++)
+            {
+                GameObject obj = Instantiate(prefab, _container.transform);
+                obj.SetActive(false);
+                _objects.Add(obj);
+            }
+        }
+        
+        protected bool TryGetObject(out GameObject obj)
+        {
+            obj = _objects.First(x => x.activeSelf == false);
+
+            return obj != null;
+        }
+    }
+}
---
+++ Assets/Scripts/Pool/PipeSpawner.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FSM.States;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Pool
+{
+    public class PipeSpawner : ObjectPool
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private float _secondsRate;
+        [SerializeField] private Transform[] _points; 
+        
+        private Coroutine _spawnRoutine;
+        private bool _isSpawnRoutineStarted;
+
+        private void Start() => Initializate(_prefab);
+
+        private void Spawn() => _spawnRoutine = StartCoroutine(SpawnRoutine());
+
+        private void Stop()
+        {
+            if (_isSpawnRoutineStarted)
+            {
+                StopCoroutine(_spawnRoutine);
+                _isSpawnRoutineStarted = false;
+            }
+        }
+
+        private void SetObject(GameObject obj, Vector3 position)
+        {
+            obj.SetActive(true);
+            obj.transform.position = position;
+        }
+        
+        private IEnumerator SpawnRoutine()
+        {
+            _isSpawnRoutineStarted = true;
+            
+            while (true)
+            {
+                if (TryGetObject(out GameObject obj))
+                {
+                    int pointIndex = Random.Range(0, _points.Length);
+                    SetObject(obj, _points[pointIndex].position);
+                    
+                    yield return new WaitForSeconds(_secondsRate);
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
